a5b400f8ef45f3ca4d04dab9fcb2429ab2d1efda
NexushasTaken <nexushastaken@gmail.com>
2026-04-02 01:30:43 +0800

    feat: add more directories to UserContextProvider

--- lib/src/contexts/user.rs
+++ lib/src/contexts/user.rs
@@ -1,6 +1,5 @@
 use crate::contexts::{Context, ContextProvider};
 use anyhow::Result;
-use dirs_next::{config_dir, data_dir, data_local_dir, document_dir, home_dir};
 
 pub struct UserContextProvider {}
 
@@ -13,41 +12,42 @@ impl ContextProvider for UserContextProvider {
         let name = whoami::realname().unwrap_or_else(|_| String::from("unknown"));
         let username = whoami::username().unwrap_or_else(|_| String::from("unknown"));
 
-        Ok(vec![
+        let mut dirs: Vec<super::Context> = vec![
+            ("audio_dir", dirs_next::audio_dir()),
+            ("cache_dir", dirs_next::cache_dir()),
+            ("config_dir", dirs_next::config_dir()),
+            ("data_dir", dirs_next::data_dir()),
+            ("data_local_dir", dirs_next::data_local_dir()),
+            ("desktop_dir", dirs_next::desktop_dir()),
+            ("document_dir", dirs_next::document_dir()),
+            ("download_dir", dirs_next::download_dir()),
+            ("executable_dir", dirs_next::executable_dir()),
+            ("font_dir", dirs_next::font_dir()),
+            ("home_dir", dirs_next::home_dir()),
+            ("picture_dir", dirs_next::picture_dir()),
+            ("public_dir", dirs_next::public_dir()),
+            ("runtime_dir", dirs_next::runtime_dir()),
+            ("template_dir", dirs_next::template_dir()),
+            ("video_dir", dirs_next::video_dir()),
+        ]
+        .iter()
+        .map(|(key, value)| {
+            Context::KeyValueContext(
+                key.to_string(),
+                value
+                    .clone()
+                    .map(Into::into)
+                    .unwrap_or_else(|| "unknown".into()),
+            )
+        })
+        .collect();
+        dirs.append(&mut vec![
             Context::KeyValueContext(String::from("id"), self.get_uid().to_string().into()),
             Context::KeyValueContext(String::from("name"), name.into()),
             Context::KeyValueContext(String::from("username"), username.into()),
-            Context::KeyValueContext(
-                String::from("home_dir"),
-                home_dir()
-                    .map(Into::into)
-                    .unwrap_or_else(|| "unknown".into()),
-            ),
-            Context::KeyValueContext(
-                String::from("config_dir"),
-                config_dir()
-                    .map(Into::into)
-                    .unwrap_or_else(|| "unknown".into()),
-            ),
-            Context::KeyValueContext(
-                String::from("data_dir"),
-                data_dir()
-                    .map(Into::into)
-                    .unwrap_or_else(|| "unknown".into()),
-            ),
-            Context::KeyValueContext(
-                String::from("data_local_dir"),
-                data_local_dir()
-                    .map(Into::into)
-                    .unwrap_or_else(|| "unknown".into()),
-            ),
-            Context::KeyValueContext(
-                String::from("document_dir"),
-                document_dir()
-                    .map(Into::into)
-                    .unwrap_or_else(|| "unknown".into()),
-            ),
-        ])
+        ]);
+
+        Ok(dirs)
     }
 }
 
